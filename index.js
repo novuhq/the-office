4dd1b027600708ba9d6a9b06de30ca9b8b480aab
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-02-15 15:38:53 +0000

    feat: Add roof banner

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const fs = require('fs');
 
 const API_KEY = process.env.GATHER_API_KEY;
 
+const GATHER_OJBECTS_ENUM = {
+    ROOF_BANNER : 'NTnj',
+}
+
+const GATHER_MAPS_ENUM = {
+    OFFICE_ROOF : 'office-roof',    
+}
+
 /**
  * References:
  * 
@@ -53,55 +61,82 @@ game.subscribeToEvent("playerMoves", (data, context) => {
 });
 
 game.subscribeToEvent(
-    "playerInteractsWithObject",
+    'mapSetObjectsV2',
+    (obj, _context) => {
+        console.log(`tree ${JSON.stringify(obj,null,2)} chopped!`);
+    });
+game.subscribeToEvent(
+    'playerTriggersObject',
     ({ playerInteractsWithObject: { key } }, _context) => {
         console.log(`tree ${key} chopped!`);
     });
-
-
-nodeHtmlToImage({
-    output: './image.png',
-    type: 'png',
-    transparent: true,
-    html: `<html>
-    <head>
-      <style>
-        body {
-          width: 158px;
-          height: 70px;
-          background: transparent;
-        }
-      </style>
-    </head>
-    <body style="padding-left: 1px; padding-top: 16px;  background: transparent">
-        <div style="background: white; display: block; padding: 10px;">
-            Hello David!
-        </div>
-    </body>
-  </html>
-  `
-})
-    .then(() => {
-        const image = fs.readFileSync('image.png');
-        const blob = put('metrics' + new Date().getMilliseconds() +'.png', image, {
-            access: 'public',
-            cacheControlMaxAge: 0
-        }).then((result) => {
-            console.log(result.url);
-
-            game.sendAction({
-                $case: "mapUpdateObjects",
-                mapUpdateObjects: {
-                    mapId: 'blank',
-                    objects: {
-                        ['2cIz']: {
-                            type: 0,
-                            normal: result.url,
-                            customState: "hole",
-                            _tags: [], // smh we're going to hopefully get rid of this soon but for now you just have to include it with setObject actions, sorry
+    
+let i = 0;
+const maxI = 10;
+const width = 300;
+const paddingIncrement = width / maxI;
+const interval = 2000;
+setInterval(() => {
+    nodeHtmlToImage({
+        output: './image.png',
+        type: 'png',
+        transparent: true,
+        html: `<html>
+        <head>
+          <style>
+            body {
+              width: ${width}px;
+              height: 70px;
+              overflow: hidden;
+              background: transparent;
+              font-family: Brother-1816,ui-sans-serif,system-ui,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;
+              font-weight: 400;
+              letter-spacing: 0.00938em;
+            }
+          </style>
+        </head>
+        <body style="padding-left: 1px; background: transparent; border: 5px solid black;">
+            <div style="overflow: hidden;">
+                <div style="width: ${width + 200}px; background: white; display: block; padding: 10px; overflow: hidden;">
+                    <div style="padding-left:${paddingIncrement* i}px; display: flex; align-items: center;">
+                        <img src="https://assets.super.so/1e9f5a51-c4c6-4fca-b6e8-25fa0186f139/uploads/favicon/d82d95bb-0983-4980-8b3f-dda6ecb0c22c.png" style="width: 30px; height: 30px; background: transparent;"/>
+                        <p style="margin-left: 10px;">HELLO NOTIFIERS!</p>
+                    </div>
+                </div>
+            </div>
+        </body>
+      </html>
+      `
+    })
+        .then(() => {
+            const image = fs.readFileSync('image.png');
+            const blob = put('metrics' + new Date().getMilliseconds() +'.png', image, {
+                access: 'public',
+                cacheControlMaxAge: 0
+            }).then((result) => {
+                console.log(result.url);
+    
+                game.sendAction({
+                    $case: "mapUpdateObjects",
+                    mapUpdateObjects: {
+                        mapId: GATHER_MAPS_ENUM.OFFICE_ROOF,
+                        objects: {
+                            [GATHER_OJBECTS_ENUM.ROOF_BANNER]: {
+                                type: 0,
+                                normal: result.url,
+                                customState: "hole",
+                                _tags: [], // smh we're going to hopefully get rid of this soon but for now you just have to include it with setObject actions, sorry
+                            },
                         },
                     },
-                },
+                });
             });
-        });
-    })
+        })
+
+    if (i === maxI) {
+        i = 0;
+    } else {
+        i++;
+    }
+}, interval);
+
